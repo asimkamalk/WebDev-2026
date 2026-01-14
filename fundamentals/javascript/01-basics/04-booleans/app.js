12319a3437b97dd5fe299193df964402e9838fab
Asim Kamal <asimkamalk@gmail.com>
2026-01-14 07:49:51 +0500

    Introduce Booleans and Falsy Values in JavaScript

---
+++ fundamentals/javascript/01-basics/04-booleans/app.js
@@ -0,0 +1,23 @@
+// Boolean
+// True || False
+
+let isTrue = true;
+console.log(isTrue); //true
+
+isTrue = false;
+console.log(isTrue); //false
+
+isTrue = NaN;
+console.log(isTrue); //NaN //Not a Number
+
+let number = 10;
+console.log(number + undefined); //NaN
+
+// falsy
+// -> false
+// -> null
+// -> undefined
+// -> 0
+// -> -0
+// -> NaN
+// -> "",  '', ``, (empty quotes)
