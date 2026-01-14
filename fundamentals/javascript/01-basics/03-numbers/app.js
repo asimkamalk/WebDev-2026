247af9daa8e5401252e5619c4f5deda65fdeb45e
Asim Kamal <asimkamalk@gmail.com>
2026-01-14 07:49:50 +0500

    Explore Numbers in JavaScript

---
+++ fundamentals/javascript/01-basics/03-numbers/app.js
@@ -0,0 +1,63 @@
+// Primitive Types
+// 1 Numbers
+
+let num = 10;
+console.log(num); //10
+console.log(typeof num); //number
+
+// floating number
+let num2 = 10.5;
+console.log(num2); //10.5
+console.log(typeof num2); //number
+
+// negative number
+let num3 = -5;
+console.log(num3); //-5
+console.log(typeof num3); //number
+
+// Basic Math
+// console.log(2 + 2); // 4
+// console.log(2 - 2); // 0
+// console.log(2 * 2); // 4
+// console.log(2 / 2); // 1
+// console.log(2 ** 2); // 4 //Power
+// console.log(2 % 2); // 0 //Modulus
+
+let counter = 0;
+
+// increment
+counter++;
+
+console.log(counter); // 1
+
+// decrement
+counter--;
+
+console.log(counter); // 0
+
+console.clear();
+
+// Excercise
+// 1. Create a variable name (firstFavNum) & store your favourite number
+// 2. Create a variable name (secondFavNum) & store your favourite number
+// 3. Add (firstFavNum & secondFavNum)
+// 4. Subtract (firstFavNum & secondFav)
+// 5. Multiply (firstFavNum & secondFav)
+// 6. Divide (firstFavNum & secondFav)
+// 7 Check (firstFavNum mod secondFav)
+// 7 Check the power of (firstFavNum)
+
+let firstFavNum = 20;
+let secondFavNum = 10;
+
+console.log(firstFavNum + secondFavNum); //30
+
+console.log(firstFavNum - secondFavNum); //10
+
+console.log(firstFavNum * secondFavNum); //200
+
+console.log(firstFavNum / secondFavNum); //2
+
+console.log(firstFavNum % secondFavNum); //0
+
+console.log(firstFavNum ** secondFavNum); //102400....
