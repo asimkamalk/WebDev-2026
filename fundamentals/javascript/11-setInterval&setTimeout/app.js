a7ad0e243c6bf1a2b1ad10bb962d15fe0535af92
Asim Kamal <asimkamalk@gmail.com>
2026-01-22 04:08:08 +0500

    Adds setInterval and setTimeout examples
    Introduces examples demonstrating the usage of `setInterval` and `setTimeout` functions in JavaScript.
    
    Illustrates how to execute code after a specified delay using `setTimeout` and how to repeatedly execute code at a fixed interval using `setInterval`.
    
    Also demonstrates how to clear an interval using `clearInterval` after a specified duration.

---
+++ fundamentals/javascript/11-setInterval&setTimeout/app.js
@@ -0,0 +1,24 @@
+// setTimeout
+
+// setInterval(function () {
+//   console.log("This function will be executed after 3 seconds");
+// }, 3000);
+
+// setInterval
+
+// setInterval(() => {
+//   console.log("This function will be executed every 2 seconds");
+// }, 2000);
+
+// setInterval with clearInterval
+
+const intervalId = setInterval(function () {
+  console.log("This function is being executed at the interval");
+}, 1000);
+
+// Stop the interval after 10 seconds
+
+setTimeout(function () {
+  clearInterval(intervalId);
+  console.log("Interval Stopped!");
+}, 10000);
