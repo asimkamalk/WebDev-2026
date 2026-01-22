12b62bc3c35e87bef894a7a9d0630dec24dc8b5e
Asim Kamal <asimkamalk@gmail.com>
2026-01-22 22:42:52 +0500

    Implements template string example
    Demonstrates the use of template strings to create a multiline string and embed variables.

--- fundamentals/javascript/12-template-strings/excersice.js
+++ fundamentals/javascript/12-template-strings/excersice.js
@@ -7,7 +7,21 @@
 // 2. Create two variables (firstName, lastName)
 // 3. Use that variables in template string.
 
+<<<<<<< Updated upstream
 console.log(`The quick
 brown fox
 jumps over
 the lazy dog`);
+=======
+const text = `The quick
+brown fox
+jumps over
+the lazy dog`;
+
+console.log(text);
+
+const firstName = "Asim";
+const lastName = "Kamal";
+
+console.log(`My name is: ${firstName} ${lastName}`);
+>>>>>>> Stashed changes
