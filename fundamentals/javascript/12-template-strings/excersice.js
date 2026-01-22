5863cc85ce50b6cbcdd710ca02ff21835c3f0ce4
Asim Kamal <asimkamalk@gmail.com>
2026-01-22 22:43:42 +0500

    Removes conflict markers
    Removes leftover conflict markers from the file, cleaning up the code.

--- fundamentals/javascript/12-template-strings/excersice.js
+++ fundamentals/javascript/12-template-strings/excersice.js
@@ -7,12 +7,6 @@
 // 2. Create two variables (firstName, lastName)
 // 3. Use that variables in template string.
 
-<<<<<<< Updated upstream
-console.log(`The quick
-brown fox
-jumps over
-the lazy dog`);
-=======
 const text = `The quick
 brown fox
 jumps over
@@ -24,4 +18,3 @@ const firstName = "Asim";
 const lastName = "Kamal";
 
 console.log(`My name is: ${firstName} ${lastName}`);
->>>>>>> Stashed changes
