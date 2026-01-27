4717af6c01edfd54c803e45702589230ae21cfa3
Asim Kamal <asimkamalk@gmail.com>
2026-01-27 23:33:48 +0500

    Add ternary operator explanation comment

--- fundamentals/javascript/22-ternary-operator/app.js
+++ fundamentals/javascript/22-ternary-operator/app.js
@@ -1,2 +1,5 @@
 // Ternary Operator
+
+// The ternary operator in JavaScript is a concise way of writing conditional expressions by a question mark (?), an expression to execute if the condition is true, followed by a colon (:), and an expression to execute if the condition is false
+
 // condition ? expressionIfTrue : expressionIfFalse;
