705e8a8b46772678200972c6189ce8e26d161a55
Asim Kamal <asimkamalk@gmail.com>
2026-01-29 04:35:05 +0500

    Add for...in loop examples for objects and arrays with comments

---
+++ fundamentals/javascript/23-for-in-loop/app.js
@@ -0,0 +1,3 @@
+// The "for... in" loop in JavaScript is used to iterate over the enumerable properties of an object. It is a way to loop through the keys (property names) of an object
+
+// for (let key in object) {...}
