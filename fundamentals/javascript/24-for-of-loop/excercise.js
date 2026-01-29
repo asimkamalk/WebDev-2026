13d7af44edcf5b6f467c439ec5f5152850dbe785
Asim Kamal <asimkamalk@gmail.com>
2026-01-29 05:00:20 +0500

    Add simple for...of loop example to iterate over array

---
+++ fundamentals/javascript/24-for-of-loop/excercise.js
@@ -0,0 +1,9 @@
+// Iterate over "array1" $ log the values using for of loop
+
+// Using a `for...of` loop to iterate over an array.
+// 'element' represents each item in the 'array1' array.
+const array1 = ["a", "b", "c"];
+
+for (const element of array1) {
+  console.log(element); // Output: a, b, c
+}
