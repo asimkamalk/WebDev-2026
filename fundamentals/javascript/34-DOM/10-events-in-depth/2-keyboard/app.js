484c4c6958bd0cc005ae25dbce97f2a317b72152
Asim Kamal <asimkamalk@gmail.com>
2026-02-24 03:07:43 +0500

    Added mouse and keyboard events

---
+++ fundamentals/javascript/34-DOM/10-events-in-depth/2-keyboard/app.js
@@ -0,0 +1,22 @@
+// https://www.w3schools.com/jsref/obj_keyboardevent.asp
+const input = document.querySelector("input");
+
+// input.addEventListener("keypress", () => {
+//   console.log("Key Pressed");
+// });
+
+// input.addEventListener("keyup", () => {
+//   console.log("Key Up");
+// });
+
+// input.addEventListener("keydown", () => {
+//   console.log("Key Down");
+// });
+
+// Useful properties and methods
+input.addEventListener("keypress", (e) => {
+  //   console.log(e.charCode);
+  //   console.log(e.code);
+  //   console.log(e.ctrlKey);
+  console.log(e.key);
+});
